644216bc3f2b8c9b7542fc6926f5b89493864c8e
misterizzo <mikedrizzo@gmail.com>
2014-06-25 01:14:48 -0700

    Added Devise & Modifed dev/prod config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Required for Heroku
+  # Note to set this to actual host
+  config.action_mailer.default_url_options = { :host => 'http://misterizzo-pinteresting.herokuapp.com/' }
 end
